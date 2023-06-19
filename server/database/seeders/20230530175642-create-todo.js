231aeae199bc7800782f4017d7759598d0adfae9
Navin Mishra <51904454+navinmishra1717@users.noreply.github.com>
2023-06-19 10:12:51 +0545

    Create todo seeder file

---
+++ server/database/seeders/20230530175642-create-todo.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      'Todos',
+      [
+        {
+          title: 'test todo one',
+          status: 'pending',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'test todo two',
+          status: 'completed',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Todos', null, {});
+  },
+};
